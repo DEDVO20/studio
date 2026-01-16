760d1b0007109e74d77d3df6f58794edd6b9740e
Deiver Andres Ordosgoitia villadiego <dedvo2016@gmail.com>
2026-01-16 21:37:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7664487125-fcdf1",
+  "appId": "1:658721470744:web:1426650d58ff1dcc75466d",
+  "apiKey": "AIzaSyB-QL8s7Fvl91UFxjPwRfLNlL4-YJNK3cM",
+  "authDomain": "studio-7664487125-fcdf1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "658721470744"
+};
